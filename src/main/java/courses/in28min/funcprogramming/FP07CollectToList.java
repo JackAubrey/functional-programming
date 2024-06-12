7f02162bdaa24520d6bba1253230ba223c14ad07
danilo <d.cividin@hotmail.it>
2024-06-12 16:05:48 +0200

    first usage of collecting using the short-cut "toList"

---
+++ src/main/java/courses/in28min/funcprogramming/FP07CollectToList.java
@@ -0,0 +1,28 @@
+package courses.in28min.funcprogramming;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class FP07CollectToList {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+        System.out.println("Calculate square of numbers and collect to other list");
+        List<Integer> squaredNumbers = calcSquare(numbers);
+
+
+        System.out.println("Original | Squared");
+        int cnt = 0;
+        for(Integer num:numbers) {
+            System.out.println(String.format("%-9s|%4s", num, squaredNumbers.get(cnt++)));
+        }
+    }
+
+    private static List<Integer> calcSquare(List<Integer> numbers) {
+        return numbers.stream()
+                .map( n -> n*n)
+                .toList();
+
+    }
+
+}
