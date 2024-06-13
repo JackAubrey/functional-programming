34f4005cb7f63ad99aa966e111c0a11a86b2d93f
danilo <d.cividin@hotmail.it>
2024-06-13 15:59:41 +0200

    exercise: a first approach to Functions

---
+++ src/main/java/courses/in28min/funcprogramming/exercises/ex_04_func/FPEx12DoReduceFunction.java
@@ -0,0 +1,17 @@
+package courses.in28min.funcprogramming.exercises.ex_04_func;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class FPEx12DoReduceFunction {
+    public static void main(String[] args) {
+        BinaryOperator<Integer> sum = (x,y) -> x+y;
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        
+        int result = numbers.stream()
+                .reduce(0, sum);
+
+        System.out.println(result);
+    }
+}
