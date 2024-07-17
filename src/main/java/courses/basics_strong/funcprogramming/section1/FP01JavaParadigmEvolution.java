0c1dcb8b9997ccf9fa229b22c91231c8d3755a58
danilo <d.cividin@hotmail.it>
2024-07-17 15:37:30 +0200

    First example of "Basics Strong" Java Functional and Reactive course

---
+++ src/main/java/courses/basics_strong/funcprogramming/section1/FP01JavaParadigmEvolution.java
@@ -0,0 +1,66 @@
+package courses.basics_strong.funcprogramming.section1;
+
+public class FP01JavaParadigmEvolution {
+    public static void main(String[] args) {
+        // Previous Java 8 for example if we need to create a runnable class we need to
+        // create a class that implements Runnable and pass it to a thread
+        seeExampleWithRunnableClass();
+
+        // thanks to anonymous classes we were able to reduce the code a bit
+        seeExampleWithRunnableAnonymousClass();
+
+        // thanks lambda introduced from Java 8 the code is very concise
+        seeExampleWithLambda();
+
+        // an example of usage of lambda on the fly
+        seeExampleWithLambdaOnTheFly();
+    }
+
+    private static void seeExampleWithRunnableClass() {
+        /**
+         * A Class that implements Runnable
+         */
+        class MyRunnable implements Runnable {
+            @Override
+            public void run() {
+                System.out.println("MyRunnable ThreadId = "+Thread.currentThread().threadId());
+            }
+        }
+
+        // Now lets go to use this class
+        Thread t = new Thread(new MyRunnable());
+        t.start();
+    }
+
+    private static void seeExampleWithRunnableAnonymousClass() {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Anonymous Runnable ThreadId = "+Thread.currentThread().threadId());
+            }
+        });
+        t.start();
+    }
+
+    private static void seeExampleWithLambda() {
+        Thread t = new Thread( () -> System.out.println("Lambda ThreadId = "+Thread.currentThread().threadId()) );
+        t.start();
+    }
+
+    private static void seeExampleWithLambdaOnTheFly() {
+        onTheFly( () -> System.out.println("Hello on the fly") );
+    }
+
+    /**
+     * Used by {@link #seeExampleWithLambdaOnTheFly()}
+     * @param func a functional interface
+     */
+    private static void onTheFly(MyFunctionalInterface func) {
+        func.myMethod();
+    }
+}
+
+@FunctionalInterface
+interface MyFunctionalInterface {
+    void myMethod();
+}
