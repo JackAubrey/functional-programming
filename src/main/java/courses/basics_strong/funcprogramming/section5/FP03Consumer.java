ee4b73827ffe7d92b92418155c2530dd12139d03
danilo <d.cividin@hotmail.it>
2024-07-19 16:36:44 +0200

    Functional Interface: Consumer

---
+++ src/main/java/courses/basics_strong/funcprogramming/section5/FP03Consumer.java
@@ -0,0 +1,25 @@
+package courses.basics_strong.funcprogramming.section5;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class FP03Consumer {
+    public static void main(String[] args) {
+        // Consumer interface abstract method signature is: "void accept(T t)"
+        // Consumer use "accept" to do something with received value and do not return anything.
+        List<String> list = List.of("Basics", "Strong", "java", "functional", "programming", "BasicsStrong");
+        List<Integer> listInt = List.of(1,5,8,90,32,45);
+        Consumer<String> printUppercaseString = s -> System.out.println(s.toUpperCase());
+        Consumer<Integer> printIsOddOrEven = i -> System.out.println( i%2==0 ? i + " is even" : i + " is odd" );
+
+        consume(list, printUppercaseString);
+        consume(listInt, printIsOddOrEven);
+    }
+
+    private static <T> void consume(List<T> list, Consumer<T> predicate) {
+        for (T s:list) {
+            predicate.accept(s);
+        }
+    }
+}
