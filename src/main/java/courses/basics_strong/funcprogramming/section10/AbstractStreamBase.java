a4c5dc78985fd198e723870ed272e67b54f5594b
danilo <d.cividin@hotmail.it>
2024-08-01 12:38:59 +0200

    Streams: The Stream Pipeline class improvement

--- src/main/java/courses/basics_strong/funcprogramming/section10/AbstractStreamBase.java
+++ src/main/java/courses/basics_strong/funcprogramming/section10/AbstractStreamBase.java
@@ -4,11 +4,13 @@ import courses.basics_strong.funcprogramming.section10.data.Book;
 import courses.basics_strong.funcprogramming.section10.data.BookBuilder;
 import net.datafaker.Faker;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 
 public abstract class AbstractStreamBase {
+    private static Random random = new Random();
 
     // this method create random data. sometimes you should run this code in order to see a result
     static List<Book> createBookList(){
@@ -23,9 +25,15 @@ public abstract class AbstractStreamBase {
         return new BookBuilder()
                 .setAuthor(bookFaker.author())
                 .setGenre(bookFaker.genre())
-                .setPrice(new Random().nextDouble(5, 35))
-                .setRating( (short) new Random().nextInt(1, 5))
+                .setPrice(randomPrice(5, 35))
+                .setRating( (short) random.nextInt(1, 5))
                 .setTitle(bookFaker.title())
                 .createBook();
+    }
+
+    static double randomPrice(int min, int max) {
+        return new BigDecimal(random.nextDouble(min, max))
+                .setScale(2, BigDecimal.ROUND_HALF_UP)
+                .doubleValue();
     }
 }
