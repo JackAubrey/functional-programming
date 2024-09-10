183b37b5090afb9224efd03d8f25a7eff6df4b01
danilo <d.cividin@hotmail.it>
2024-09-10 17:05:02 +0200

    Added utility method

--- src/main/java/courses/basics_strong/reactive/BasicExampleClass.java
+++ src/main/java/courses/basics_strong/reactive/BasicExampleClass.java
@@ -2,9 +2,15 @@ package courses.basics_strong.reactive;
 
 import io.reactivex.rxjava3.annotations.NonNull;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public abstract class BasicExampleClass {
+    private static final Random random = new Random();
+
+    protected long nextRandomLong(long from, long to) {
+        return random.nextLong(from, to);
+    }
     protected static void log(@NonNull Object e) {
         System.out.println(e);
     }
