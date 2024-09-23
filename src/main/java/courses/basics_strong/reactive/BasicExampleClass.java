d16f758176c8783ee1a0eaddb899366e02ddb345
danilo <d.cividin@hotmail.it>
2024-09-23 16:16:11 +0200

    added log-back

--- src/main/java/courses/basics_strong/reactive/BasicExampleClass.java
+++ src/main/java/courses/basics_strong/reactive/BasicExampleClass.java
@@ -1,22 +1,33 @@
 package courses.basics_strong.reactive;
 
 import io.reactivex.rxjava3.annotations.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public abstract class BasicExampleClass {
+    private static final Logger logger = LoggerFactory.getLogger(BasicExampleClass.class);
     private static final Random random = new Random();
+
+    protected BasicExampleClass() {
+        super();
+    }
 
     protected static long nextRandomLong(long from, long to) {
         return random.nextLong(from, to);
     }
     protected static void log(@NonNull Object e) {
-        System.out.println(e);
+        logger.info("{}", e);
     }
 
-    protected static void logErr(@NonNull Object e) {
-        System.err.println(e);
+    protected static void logErr(@NonNull String e) {
+        logger.error(e);
+    }
+
+    protected static void logErr(@NonNull Throwable t) {
+        logger.error("Error: ", t);
     }
 
     protected static void sleep(long period, @NonNull TimeUnit unit) {
